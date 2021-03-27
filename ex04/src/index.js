08b90741702e65aabe3f33d17930df09c904dad5
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-27 09:52:30 +0100

    Solution for the example 4

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <h1>Hello, Padawans!</h1>
+    </div>
+    )
+  }
+}
+
+// add the following code below this line
+ReactDOM.render(<App />, document.getElementById('root'));
